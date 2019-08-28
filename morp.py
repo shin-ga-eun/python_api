9782180dfad58ad003855b1f713965266799d3a9
신가은 <sge7102@naver.com>
2019-08-28 17:53:09 +0900

    모듈 만들기.

--- morp.py
+++ morp.py
@@ -4,20 +4,19 @@ import urllib3
 import json
 from collections import OrderedDict
 from pprint import pprint 
- 
-openApiURL = "http://aiopen.etri.re.kr:8000/WiseNLU" 
-accessKey = "9cf5e5b7-55b3-4369-9921-726d59b3b6e5" #인증키
-analysisCode = "ner" 
+import Analysis
+
 # 형태소 분석 : “morp”,
 # 어휘의미 분석 (동음이의어 분석) : “wsd”
 # 어휘의미 분석 (다의어 분석) : “wsd_poly”
 # 개체명 인식 : “ner”
 # 의존 구문 분석 : “dparse”
 # 의미역 인식 : “srl”
+ 
+openApiURL = "http://aiopen.etri.re.kr:8000/WiseNLU" 
+accessKey = "9cf5e5b7-55b3-4369-9921-726d59b3b6e5" #인증키
 
-# 입력(분석대상)
-myFile = open("C:\\Users\\sge\\python\\example.txt","r", encoding="utf-8")
-text = myFile.read()
+(text, analysisCode) = Analysis.fileInput()
 
 requestJson = {
     "access_key": accessKey,
@@ -63,11 +62,7 @@ for i in range(0,num):
     pprint(sentence[i]['SRL'])
 
 
-
-#결과값을 json파일로 저장
-with open('morp.json','w', encoding="utf-8") as make_file:
-   json.dump(test, make_file, ensure_ascii=False)
-
+Analysis.jsonOutput(test)
 
 
 
