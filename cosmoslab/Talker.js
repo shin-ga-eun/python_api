46740c4420f16046c35495821e529b1583e60a95
신가은 <sge7102@naver.com>
2019-09-25 15:14:29 +0900

    first

--- cosmoslab/Talker.js
+++ cosmoslab/Talker.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-class Talker extends Component {
+export default class Talker extends Component {
     render() {
         return (
             <div>
@@ -9,4 +9,3 @@ class Talker extends Component {
         )
     }
 }
-export default Talker;
